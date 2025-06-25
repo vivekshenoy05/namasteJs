3b3b66c229ca6d1534c6aa8004804560d5180161
vivekshenoy05 <vivekshenoy2000@gmail.com>
2025-06-25 23:43:17 +0530

    issue with callback

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# namasteJs
+# Namaste JavaScript
+
+This repository contains notes and examples based on the **Namaste JavaScript** series.
+
+---
+
+## 📌 Callbacks
+
+### ✅ Benefits of Callbacks
+
+- Helps in handling asynchronous operations.
+- Allows functions to be passed as arguments and executed later.
+
+### ⚠️ Drawbacks of Callbacks
+
+1. **Callback Hell**  
+   Nested callbacks make code difficult to read and maintain.
+
+2. **Inversion of Control**  
+   You lose control of the flow when you pass your callback to another function, especially if it decides **when**, **how many times**, or **whether at all** to call it.
+
+---
+
+
--- index.js
+++ index.js
@@ -1,54 +1,85 @@
-// tricky map()
+// Js executes the program line by line but in case if you want to execute randomly 
+// we can acheive this by using callback function and pasing this function to setTimeout
+// asynchronous program in js exists because callback exists
 
-const users = [
-    {firstName: 'Vivek', lastName: 'Shenoy', age: 25},
-    {firstName: 'ram', lastName: 'Shenoy', age: 25},
-    {firstName: 'roopa', lastName: 'Shenoy', age: 22},
+console.log('hello');
+console.log('JS');
+console.log('world');
 
-];
+// suppose you want to execute world at last 
 
-// need to find list of full name of all the persons in the array
-const output = users.map(x=>x.firstName + ' '+ x.lastName);
-console.log(output);
+console.log('hello');
+function hi(){
+ console.log('JS');
+}
 
-// find the people with the same age {25: 2, 22: 1}
-// we are using reduce method here since we want one object and that object should have different
-//  unique value
-// here initial value of accumulator will be object because when there is no data prsent in it there will be empty object
+setTimeout(hi,10000);
 
-const output1 = users.reduce((acc,cur)=>{
-    if(acc[cur.age]){
-        acc[cur.age] = ++acc[cur.age]
-    }else{
-        acc[cur.age] = 1;
-    }
-    return acc;
-},{});
-
-console.log(output1);
-
-// now we need to find first name of all the people whose age is less than 24
-
-const output2 = users.filter(x=>x.age>24);
-console.log(output2);
+console.log('world');
 
 // output
-// [0: {firstName: 'Vivek', lastName: 'Shenoy', age: 25}
-// 1: {firstName: 'ram', lastName: 'Shenoy', age: 25}]
+// hello  world js
+// using callback its a powerful way to do asynchronous operations in js
 
-// this returns the arry with age > 24 but we need to find only firstName here chaing comes into picture
-// map will be running on the result of reduce method
+// will take eg of ecommerce website
+// in ecommerce as we all know we will create order first and then we will process tio payment
+// first createOrder() api will be called and then proceedToPayment()
+// there is dependency between them and this is async operation
+// here callback comes into picture and callback helps us to write the code
 
-const output3 = users.filter(x=>x.age>24).map(x=>x.firstName);
-console.log(output3);
+// a very common pattern which we follow in programming
+// we will wrap the proceedToPayment api inside callback function
+// this callback function is passed as parameter to createOrder api\
+// now its the job of createOrder api to create the order and then call proceedToPayment method
 
-// find the same using reduce
+const cart = ['apple', 'mango', 'pant'];
 
-const output4 = users.reduce((acc,cur)=>{
-  cur.age>24 ? acc.push(cur.firstName): '';
-  return acc;
-},[]);
+api.createOrder(cart, function(){
+    api.proceedToPayment()
+});
 
-console.log(output4)
+// now if the payment is successful we need to show the order summary page
+// and for this we have api called as showOrderSummary() and this needs to called only after payment is successful
+// in this case we will have callback function and then we will pass this as parameter to proceedToPayment()
+// now its the job of proceed to payment api to complete the payment and call the showOrderSummary() function
+
+api.createOrder(cart, function(){
+    api.proceedToPayment(function(){
+        api.showOrderSummary()
+    })
+});
+
+//  now will see the problems that we might face
+// now we have created the order, payment, show order summary
+// now we need to update the wallet
+// in this case also we need to again we need to pass callback function to showOrderSummarty()
+
+api.createOrder(cart, function(){
+    api.proceedToPayment(function(){
+        api.showOrderSummary(function(){
+            api.updateWallet()
+        })
+    })
+});
+
+// so when we have large no of apis or code base this chaining keeps on increasing
+// if apis are dependent on each other we end up falling in callBack hell
+// our code starts to grow horizontally instead of vertically
+// this type of code is unmaintainable and unreadable
+// this horizontal structure is also known as pyramid of doom
 
 
+// now will see inversion of control
+// inversion of control is another problem we see using callbck
+// inversion of control means you loose the control of your code when we are using callbacks
+
+api.createOrder(cart, function(){
+    api.proceedToPayment()
+});
+
+// in the above code we take the callback function and give it to createOrder api
+// now we are blindly trusting the createOrder api
+// we are assuming that createOrder api will create the order and then call proceedToPayment isn't it risky?
+// we gave the control of the program to createOrder api, there might be many bugs inside createOrder api
+// what is our callback function is never called
+// what if our callback function is called twice?
